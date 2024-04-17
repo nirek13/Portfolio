1b06786c807e51d4245cef94af2919e1e9a09f8e
Nirek Shetty <63679448+nirek13@users.noreply.github.com>
2024-04-17 17:20:35 -0400

    Add files via upload

--- assets/js/mousefollow.js
+++ assets/js/mousefollow.js
@@ -3,31 +3,22 @@ const fullname = document.getElementById("fullname");
 const isTouchDevice = ('ontouchstart' in window || 'onmsgesturechange' in window);
 
 if (!isTouchDevice && frontpage && fullname) {
-    const xOrigin = frontpage.width() / 2;
-    const yOrigin = frontpage.height() / 2;
-    const maxRotation = 20; // Maximum rotation angle
-    const maxShadowOffset = 10; // Maximum drop shadow offset
-    frontpage.on('mousemove', (e) => {
-        const xRot = ((e.clientY - yOrigin) / yOrigin * -maxRotation);
-        const yRot = ((e.clientX - xOrigin) / xOrigin * maxRotation);
-        transform(xRot, yRot);
-        clearTimeout(thread);
-        thread = setTimeout(onmousestop, 1500);
-    });
+	const xOrigin = frontpage.width() / 2;
+	const yOrigin = frontpage.height() / 2;
+	frontpage.on('mousemove', (e) => {
+		transform(((e.clientY - yOrigin) / yOrigin * -15 + 10), ((e.clientX - xOrigin) / xOrigin * 15));
+		clearTimeout(thread);
+		thread = setTimeout(onmousestop, 1500);
+	});
 
-    let onmousestop = function () {
-        transform(0, 0);
-    }, thread;
+	let onmousestop = function () {
+		transform(0, 0);
+	}, thread;
 
-    let transform = function (xRot, yRot) {
-        fullname.animate({
-            transform: `rotateX(${yRot * 0.4}deg) rotateY(${xRot * 0.4}deg)`,
-            filter: `drop-shadow(${clamp(-maxShadowOffset, yRot * -0.5, maxShadowOffset)}px ${clamp(-maxShadowOffset, xRot * 0.5, maxShadowOffset)}px 0px var(--name-shadow))`
-        }, { duration: 250, fill: "forwards" });
-    }
-}
-
-// Helper function to clamp a value between a minimum and maximum
-function clamp(min, value, max) {
-    return Math.min(Math.max(value, min), max);
+	let transform = function (xRot, yRot) {
+		fullname.animate({
+			transform: `rotateX(${yRot * 0.4}deg) rotateY(${xRot * 0.4}deg)`,
+			filter: `drop-shadow(clamp(-8px, ${yRot * -0.5}px, 8px) clamp(-8px, ${xRot * 0.5}px, 8px) 0px var(--name-shadow))`
+		}, { duration: 250, fill: "forwards" });
+	}
 }
